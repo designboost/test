4900a8924a1feb4b4bf983fccd65e61751023372
designboost <s_benfer@hotmail.com>
2013-03-14 20:46:45 +0100

    added reservation

---
+++ lesson01/src/Reservation.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author susdesign
+ *
+ */
+public class Reservation {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
